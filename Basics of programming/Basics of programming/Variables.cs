0e7cc6748b210adc0a82d80c39f8eb8317435199
Rowtsar <uoanton@mail.ru>
2023-11-16 09:16:05 +0300

    VaribleHomeWork

---
+++ Basics of programming/Basics of programming/Variables.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics_of_programming
+{
+    internal class Variables
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Антон";
+            char secondName = 'K';
+            byte age = 26;
+            sbyte money = -100;
+            float weight = 76.5f;
+            long height = 175;
+            bool student = true;
+            short shoeSize = 42;
+            ushort countPhone = 1;
+            uint countLaptop = 1;
+        }
+    }
+}
